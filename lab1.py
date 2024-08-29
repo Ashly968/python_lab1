c171b7346609425613375f1c5c87f454f62fb1bd
Ashly968 <ashlyangon1@gmail.com>
2024-08-29 12:33:30 -0700

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,30 @@
+import math
+
+area = math.pi * 5 ** 2
+print(area)
+
+volume = 3/4 * math.pi * 3 ** 2
+print(volume)
+
+hypotenuse = math.sqrt(4) + math.sqrt(3)
+print(hypotenuse)
+
+print(str('Ashly Paola Razo Angon'))
+
+print(len('Ashly Paola Razo Angon'))
+
+print(str('ashly' + 'paola' + 'razo' + 'angon'))
+
+print(str.upper('ashly'))
+
+print(str.lower('ASHLY'))
+
+age = 20
+print(type(20))
+height = 62.4
+print(type(62.4))
+weight = 152.119
+print(type(152.119))
+BMI= weight / height * 730
+print(BMI)
+
